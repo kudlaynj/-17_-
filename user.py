08454ae424e2ca78751e03c5fe6b557652bab9e4
kudlaynj <167016811+kudlaynj@users.noreply.github.com>
2024-09-30 10:07:37 +0300

    models

---
+++ user.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter
+from app.backend.db import Base
+from app.backend.db import Column, Integer, String
+from sqlalchemy.orm import relationship
+from app.models import *
+import alembic
+
+router = APIRouter(prefix="/user", tags=["user"])
+
+
+class User(Base):
+    __tablename__ = "users"
+    __table_args__ = {'extend_existing': True}
+    is_active = bool
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    firstname = Column(String)
+    lastname = Column(String)
+    age = Column(Integer)
+    slug = Column(String, unique=True, index=True)
+    tasks = relationship("Task", back_populates="user")
+
+
+from sqlalchemy.schema import CreateTable
+
+print(CreateTable(User.__table__))
